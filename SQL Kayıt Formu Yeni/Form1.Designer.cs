d3ae291fd2eba74830363edd8ba45a496dbed9e9
LonelyOne0 <toksuneyup781@gmail.com>
2024-11-14 18:53:07 +0300

    Proje dosyası ekle.

---
+++ SQL Kayıt Formu Yeni/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace SQL_Kayıt_Formu_Yeni
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.txt_tc = new System.Windows.Forms.TextBox();
+            this.txt_dogumtarihi = new System.Windows.Forms.TextBox();
+            this.txt_memleket = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(320, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hoşgeldiniz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(198, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(198, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(198, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "T.C:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(198, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Doğum Tarihi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(198, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Memleket:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Location = new System.Drawing.Point(325, 86);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(127, 20);
+            this.txt_ad.TabIndex = 6;
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(325, 112);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(127, 20);
+            this.txt_soyad.TabIndex = 7;
+            this.txt_soyad.TextChanged += new System.EventHandler(this.txt_soyad_TextChanged);
+            // 
+            // txt_tc
+            // 
+            this.txt_tc.Location = new System.Drawing.Point(325, 138);
+            this.txt_tc.Name = "txt_tc";
+            this.txt_tc.Size = new System.Drawing.Size(127, 20);
+            this.txt_tc.TabIndex = 8;
+            // 
+            // txt_dogumtarihi
+            // 
+            this.txt_dogumtarihi.Location = new System.Drawing.Point(325, 164);
+            this.txt_dogumtarihi.Name = "txt_dogumtarihi";
+            this.txt_dogumtarihi.Size = new System.Drawing.Size(127, 20);
+            this.txt_dogumtarihi.TabIndex = 9;
+            // 
+            // txt_memleket
+            // 
+            this.txt_memleket.Location = new System.Drawing.Point(325, 190);
+            this.txt_memleket.Name = "txt_memleket";
+            this.txt_memleket.Size = new System.Drawing.Size(127, 20);
+            this.txt_memleket.TabIndex = 10;
+            this.txt_memleket.TextChanged += new System.EventHandler(this.txt_memleket_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(482, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 48);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Başvuru Yapanları Göster";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 246);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(777, 187);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.AutoSizeColumnsModeChanged += new System.Windows.Forms.DataGridViewAutoSizeColumnsModeEventHandler(this.dataGridView1_AutoSizeColumnsModeChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(482, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 48);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Başvuruyu Tamamla";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_memleket);
+            this.Controls.Add(this.txt_dogumtarihi);
+            this.Controls.Add(this.txt_tc);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kayıt Formu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.TextBox txt_tc;
+        private System.Windows.Forms.TextBox txt_dogumtarihi;
+        private System.Windows.Forms.TextBox txt_memleket;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SQL Kayıt Formu Yeni/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Kayıt_Formu_Yeni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string constring = "Data Source=DESKTOP-LATR9CC\\SQLEXPRESS;Initial Catalog=eyyubıxkayit;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(constring);
+
+        public void verilerigoster(string veriler)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(veriler, connect);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerigoster("SELECT * FROM eyyubixkisiler");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_soyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_memleket_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO eyyubixkisiler (tc, ad, soyad, dogumtarihi, memleket) VALUES (@tc, @ad, @soyad, @dogumtarihi, @memleket)", connect);
+            komut.Parameters.AddWithValue("@tc", txt_tc.Text);
+            komut.Parameters.AddWithValue("@ad", txt_ad.Text);
+            komut.Parameters.AddWithValue("@soyad", txt_soyad.Text);
+            komut.Parameters.AddWithValue("@dogumtarihi", txt_dogumtarihi.Text);
+            komut.Parameters.AddWithValue("@memleket", txt_memleket.Text);
+            komut.ExecuteNonQuery();
+            verilerigoster("SELECT * FROM eyyubixkisiler");
+            connect.Close();
+
+            txt_tc.Clear();
+            txt_ad.Clear();
+            txt_soyad.Clear();
+            txt_dogumtarihi.Clear();
+            txt_memleket.Clear();
+            txt_ad.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txt_basvurusil_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_AutoSizeColumnsModeChanged(object sender, DataGridViewAutoSizeColumnsModeEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
